b96c660b493bd92e5dc156b1bf6c877d886a9a52
Insaf <61574963+InsafHW@users.noreply.github.com>
2022-02-04 22:39:06 +0300

    Changed connection settings to default

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 5000
 
 const start = async () => {
     try {
-        await mongoose.connect('mongodb+srv://insafHw:Insaf55605@cluster0.zuznz.mongodb.net/roles_auth_pro_node_js?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://<login>:<password>@cluster0.zuznz.mongodb.net/<db_name>?retryWrites=true&w=majority')
         app.listen(5000, () => {
             console.log(`Server has been started on port=${PORT}`)
         })
